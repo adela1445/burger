2369041c53047230cdacf2c91b6701414b1ae80f
Bluebird <stephany1445@hotmail.com>
2020-10-07 17:37:14 -0700

    still tryingt to deploy

--- models/burger.js
+++ models/burger.js
@@ -1,13 +1,12 @@
-// Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-const burger = {
+// MODELS FOR BURGER
+let burger = {
   all: function (cb) {
     orm.all("burgers", function (res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
   create: function (cols, vals, cb) {
     orm.create("burgers", cols, vals, function (res) {
       cb(res);
@@ -20,5 +19,5 @@ const burger = {
   },
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export the database functions for the controller (burgersController.js).
 module.exports = burger;
--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,40 +1,35 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function () {
-  $(".change-devour").on("click", function (event) {
-    const id = $(this).data("id");
-    const newDevour = $(this).data("newdevour");
+  // ON CLICK TO CHANGE DEVOURED BOOLEAN
+  $(".change-devoured").on("click", function (event) {
+    let id = $(this).data("id");
+    let newDevour = $(this).data("newdevoured");
 
-    const newDevourState = {
+    let newDevourState = {
       devoured: newDevour,
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newDevourState,
     }).then(function () {
-      console.log("changed devour to", newDevour);
-      // Reload the page to get the updated list
       location.reload();
     });
   });
 
-  $(".create-form").on("submit", function (event) {
-    // Make sure to preventDefault on a submit event.
+  // ON SUBMIT GRAB USER INFORMATION FOR BURGER NAME
+  $(".form").on("submit", function (event) {
     event.preventDefault();
 
-    const newBurger = {
-      burger_name: $("#br").val().trim(),
-      devoured: $("[name=devoured]:checked").val().trim(),
+    let newBurger = {
+      burger_name: $("#burgerName").val().trim(),
+      devoured: false,
     };
-
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger,
     }).then(function () {
-      console.log("created new burger entry");
-      // Reload the page to get the updated list
+      console.log("Successfully created a new burger!");
       location.reload();
     });
   });
